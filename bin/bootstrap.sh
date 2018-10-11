f19a979442c86b749a40e52a836a64f6fdab48c5
Ben McManus <user@server.loc>
2018-10-11 22:20:34 +0100

    Initial commit

---
+++ bin/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+ ## Install or Update Homebrew
+echo 'Installing or Updating Homebrew...'
+set +x
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+echo -e "\n\n"
+ ## Install or Update Ansible ##
+echo 'Installing or Updating Ansible...'
+which -s ansible-playbook
+if [[ $? != 0 ]] ; then
+    brew install ansible
+fi
+echo -e "\n\n"
+
+set +a
